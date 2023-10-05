2dff092f2f2a6387a5d96327dec7f9f9fff17912
Fernando Codolo Pagani <fernandocpagani@hotmail.com>
2023-10-05 11:56:49 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -32,7 +32,7 @@ public class Board {
 	
 	public Piece piece (Position position) {
 		if (!positionExists(position)) {
-			throw new BoardException("Posição não esta no tabuleiro");
+			throw new BoardException("Posição não está no tabuleiro");
 		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
@@ -43,6 +43,19 @@ public class Board {
 		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+	
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Posição não está no tabuleiro");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
 	}
 	
 	private boolean positionExists(int row, int column) {
